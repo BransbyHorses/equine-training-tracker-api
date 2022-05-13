05f222a1ee87c141a59fcd0d98f64e686956517d
michaelthorpe <michael.thorpe@digital.homeoffice.gov.uk>
2022-05-13 10:49:38 +0100

    init app

---
+++ src/main/java/co/uk/bransby/equinetrainingtrackerapi/EquineTrainingTrackerApiApplication.java
@@ -0,0 +1,13 @@
+package co.uk.bransby.equinetrainingtrackerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EquineTrainingTrackerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EquineTrainingTrackerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/uk/bransby/equinetrainingtrackerapi/EquineTrainingTrackerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package co.uk.bransby.equinetrainingtrackerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EquineTrainingTrackerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
