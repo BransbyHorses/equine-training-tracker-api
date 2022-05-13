85cd260a2f8a706e1bad7251b99b7ce6233c60de
Petar Simanovic <petarsimanovic@hod117682.local>
2022-05-13 15:28:18 +0100

    Skill model added

--- src/main/java/co/uk/bransby/equinetrainingtrackerapi/models/Skill.java
+++ src/main/java/co/uk/bransby/equinetrainingtrackerapi/models/Skill.java
@@ -1,2 +1,23 @@
-package co.uk.bransby.equinetrainingtrackerapi.models;public class Skill {
+package co.uk.bransby.equinetrainingtrackerapi.models;
+
+import lombok.*;
+import org.hibernate.Hibernate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Objects;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "skills")
+public class Skill {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
 }
