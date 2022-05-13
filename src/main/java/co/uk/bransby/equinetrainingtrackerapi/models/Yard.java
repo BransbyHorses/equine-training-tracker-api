c2e23b1b8502fc575844d43f31bf8736fe3294b6
michaelthorpe <michael.thorpe@digital.homeoffice.gov.uk>
2022-05-13 14:41:46 +0100

    create yard entity file

---
+++ src/main/java/co/uk/bransby/equinetrainingtrackerapi/models/Yard.java
@@ -0,0 +1,40 @@
+package co.uk.bransby.equinetrainingtrackerapi.models;
+
+import lombok.*;
+import org.hibernate.Hibernate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Objects;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+@Entity(name = "yards")
+public class Yard {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+//    @OneToMany
+//    @JoinColumn(name = "cognito_id")
+//    private List<Trainer> trainers;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        Yard yard = (Yard) o;
+        return id != null && Objects.equals(id, yard.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
+}
