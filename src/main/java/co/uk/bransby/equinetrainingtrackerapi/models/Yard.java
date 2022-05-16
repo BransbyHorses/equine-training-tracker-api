340eb3e167c535b5a88c3588378f5a68f9a64dae
michaelthorpe <michael.thorpe@digital.homeoffice.gov.uk>
2022-05-16 15:27:55 +0100

    add table annotation to yard class

--- src/main/java/co/uk/bransby/equinetrainingtrackerapi/models/Yard.java
+++ src/main/java/co/uk/bransby/equinetrainingtrackerapi/models/Yard.java
@@ -3,10 +3,7 @@ package co.uk.bransby.equinetrainingtrackerapi.models;
 import lombok.*;
 import org.hibernate.Hibernate;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.util.Objects;
 
 @NoArgsConstructor
@@ -15,7 +12,8 @@ import java.util.Objects;
 @Setter
 @ToString
 @RequiredArgsConstructor
-@Entity(name = "yards")
+@Entity(name = "Yard")
+@Table(name="yards")
 public class Yard {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
