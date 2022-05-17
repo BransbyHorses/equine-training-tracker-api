5ab4295210343616f7daa3507516368cdce4ffd5
michaelthorpe <michael.thorpe@digital.homeoffice.gov.uk>
2022-05-17 14:59:44 +0100

    change model annotations

--- src/main/java/co/uk/bransby/equinetrainingtrackerapi/models/Category.java
+++ src/main/java/co/uk/bransby/equinetrainingtrackerapi/models/Category.java
@@ -3,17 +3,15 @@ package co.uk.bransby.equinetrainingtrackerapi.models;
 import lombok.*;
 import org.hibernate.Hibernate;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.util.Objects;
 
 @Getter
 @Setter
 @ToString
 @RequiredArgsConstructor
-@Entity(name="categories")
+@Entity
+@Table(name="CATEGORIES")
 public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
