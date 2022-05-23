d7c02b5a881c1310689f7d002396f7fb6ce508b0
michaelthorpe <michael.thorpe@digital.homeoffice.gov.uk>
2022-05-23 16:03:54 +0100

    clean up YardController file

--- src/main/java/co/uk/bransby/equinetrainingtrackerapi/controllers/YardController.java
+++ src/main/java/co/uk/bransby/equinetrainingtrackerapi/controllers/YardController.java
@@ -2,17 +2,13 @@ package co.uk.bransby.equinetrainingtrackerapi.controllers;
 
 import co.uk.bransby.equinetrainingtrackerapi.models.Yard;
 import co.uk.bransby.equinetrainingtrackerapi.services.YardService;
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.persistence.EntityNotFoundException;
-import java.net.URI;
-import java.net.URISyntaxException;
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/data/yards")
@@ -24,7 +20,6 @@ public class YardController {
         this.yardService = yardService;
     }
 
-    // read yards
     @GetMapping
     @RequestMapping("{id}")
     public ResponseEntity<Yard> findYard(@PathVariable Long id) {
@@ -36,21 +31,21 @@ public class YardController {
             return new ResponseEntity<>(resHeaders, HttpStatus.NOT_FOUND);
         }
     }
-    // read all yards
+
     @GetMapping
     public ResponseEntity<List<Yard>> findAllYards() {
         List<Yard> allYards = yardService.getAllYards();
         HttpHeaders resHeaders = new HttpHeaders();
         return new ResponseEntity<List<Yard>>(allYards, resHeaders, HttpStatus.OK);
     }
-    // create yard
+
     @PostMapping
     public ResponseEntity<Yard> createYard(@RequestBody Yard yard) {
         Yard newYard = yardService.createYard(yard);
         HttpHeaders resHeaders = new HttpHeaders();
         return new ResponseEntity<Yard>(newYard, resHeaders, HttpStatus.CREATED);
     }
-    // update yard
+
     @RequestMapping(value = "{id}", method = RequestMethod.PUT)
     public ResponseEntity<Yard> updateYard(@PathVariable Long id, @RequestBody Yard updatedYardValues) {
         HttpHeaders resHeaders = new HttpHeaders();
@@ -62,11 +57,11 @@ public class YardController {
             return new ResponseEntity<>(resHeaders, HttpStatus.NOT_FOUND);
         }
     }
-    // delete yard
+
     @RequestMapping(value = "{id}", method = RequestMethod.DELETE)
-    public ResponseEntity deleteYard(@PathVariable Long id) {
+    public ResponseEntity<> deleteYard(@PathVariable Long id) {
         HttpHeaders resHeaders = new HttpHeaders();
         yardService.deleteYard(id);
-        return new ResponseEntity(resHeaders, HttpStatus.OK);
+        return new ResponseEntity<>(resHeaders, HttpStatus.OK);
     }
 }
