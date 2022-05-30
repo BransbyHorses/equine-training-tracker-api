51d5dd573093b5d7793418a96f2a5fc840086506
michaelthorpe <michael.thorpe@digital.homeoffice.gov.uk>
2022-05-30 16:18:19 +0100

    create test profile and update env variables

--- src/test/java/co/uk/bransby/equinetrainingtrackerapi/EquineTrainingTrackerApiApplicationTests.java
+++ src/test/java/co/uk/bransby/equinetrainingtrackerapi/EquineTrainingTrackerApiApplicationTests.java
@@ -2,12 +2,13 @@ package co.uk.bransby.equinetrainingtrackerapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class EquineTrainingTrackerApiApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
 	}
-
 }
