bb9f639d7731f59f451ff39d312aa4bbeb2ef407
michaelthorpe <michael.thorpe@digital.homeoffice.gov.uk>
2022-06-13 12:54:43 +0100

    add programme lombok constructors

--- src/main/java/co/uk/bransby/equinetrainingtrackerapi/models/dto/ProgrammeDTO.java
+++ src/main/java/co/uk/bransby/equinetrainingtrackerapi/models/dto/ProgrammeDTO.java
@@ -1,7 +1,11 @@
 package co.uk.bransby.equinetrainingtrackerapi.models.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@AllArgsConstructor
+@NoArgsConstructor
 @Data
 public class ProgrammeDTO {
     private Long id;
