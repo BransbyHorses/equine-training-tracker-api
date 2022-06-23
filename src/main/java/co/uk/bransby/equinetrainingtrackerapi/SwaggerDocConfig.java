10e5e58e3e1efb51eaec165325d67c3ba5545cc1
michaelthorpe <michael.thorpe@digital.homeoffice.gov.uk>
2022-06-23 14:58:12 +0100

    move swagger config file to app folder

--- src/main/java/co/uk/bransby/equinetrainingtrackerapi/SwaggerDocConfig.java
+++ src/main/java/co/uk/bransby/equinetrainingtrackerapi/SwaggerDocConfig.java
@@ -1,4 +1,4 @@
-package co.uk.bransby.equinetrainingtrackerapi.controllers;
+package co.uk.bransby.equinetrainingtrackerapi;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
