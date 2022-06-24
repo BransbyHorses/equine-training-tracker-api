2a4cd64e5916f85223d61b96d4a1a06332ba15ff
PetarSimonovic <petar@simonovic.com>
2022-06-24 10:25:34 +0100

    extra unit tests

--- src/test/java/co/uk/bransby/equinetrainingtrackerapi/services/SkillServiceTest.java
+++ src/test/java/co/uk/bransby/equinetrainingtrackerapi/services/SkillServiceTest.java
@@ -1,71 +1,72 @@
 package co.uk.bransby.equinetrainingtrackerapi.services;
 
-import co.uk.bransby.equinetrainingtrackerapi.controllers.SkillController;
 import co.uk.bransby.equinetrainingtrackerapi.models.Skill;
 import co.uk.bransby.equinetrainingtrackerapi.repositories.SkillRepository;
-import co.uk.bransby.equinetrainingtrackerapi.services.SkillService;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.aspectj.lang.annotation.Before;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.web.servlet.MockMvc;
-
-import javax.persistence.Entity;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.*;
+import static org.mockito.BDDMockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class SkillServiceTest {
+
+    private final String skillTestInstanceName = "Skill service can service skills";
 
     @Mock
     private SkillRepository skillRepository;
 
     @InjectMocks
-    private SkillService skillService;
+    private SkillService skillServiceTest;
+    private Skill skillTestInstance;
+
+    @BeforeEach
+    void setUp() {
+        skillServiceTest = new SkillService(skillRepository);
+        skillTestInstance = new Skill(1L, skillTestInstanceName);
+    }
+
+    @Test
+    void canFindAllSkills() {
+        skillServiceTest.findAll();
+        Mockito.verify(skillRepository).findAll();
+    }
 
 
     @Test
-    void savesSkill() {
+    void canSaveASkill() {
 
-        Skill skill = new Skill("Horse can be saved");
+        given(skillRepository.save(skillTestInstance)).willReturn(skillTestInstance);
 
-     //   given(skillRepository.findById(skill.getId())).willReturn(Optional.empty());
-
-        given(skillRepository.save(skill)).willReturn(skill);
-
-        Skill savedSkill = skillService.create(skill);
+        Skill savedSkill = skillServiceTest.create(skillTestInstance);
 
         assertThat(savedSkill).isNotNull();
 
 
     }
 
-    void findsAllSkills() {
-        Skill skillOne = new Skill("A skill can be found");
-        Skill skillTwo = new Skill("All skills can be found");
 
-        given(skillRepository.findAll()).willReturn(List.of(skillOne, skillTwo));
+    @Test
+    void canUpdateSkill() {
+        String updatedName = "This skill's name has been updated";
+        when(skillRepository.save(any(Skill.class))).thenReturn(skillTestInstance);
+        Skill addedSkill = skillServiceTest.create(skillTestInstance);
+        assertThat(addedSkill.getName()).isEqualTo(skillTestInstanceName);
+        addedSkill.setName(updatedName);
+        Skill updatedSkill = skillServiceTest.update(skillTestInstance, skillTestInstance.getId());
+        assertThat(updatedSkill.getId()).isEqualTo(skillTestInstance.getId());
+        assertThat(updatedSkill.getName()).isEqualTo(updatedName);
 
-        List<Skill> skillsList = skillRepository.findAll();
-
-        assertThat(skillsList).isNotNull();
-        assertThat(skillsList).hasSize(2);
     }
+
+
+
 
 
 
