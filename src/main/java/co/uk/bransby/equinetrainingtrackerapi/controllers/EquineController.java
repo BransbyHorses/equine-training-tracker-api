fe3faee47c30625806231c74aac5df52732f5bff
michaelthorpe <michael.thorpe@digital.homeoffice.gov.uk>
2022-07-08 15:56:33 +0100

    adds endpoints and logic for creating relationships with equine

--- src/main/java/co/uk/bransby/equinetrainingtrackerapi/controllers/EquineController.java
+++ src/main/java/co/uk/bransby/equinetrainingtrackerapi/controllers/EquineController.java
@@ -1,7 +1,7 @@
 package co.uk.bransby.equinetrainingtrackerapi.controllers;
 
-import co.uk.bransby.equinetrainingtrackerapi.models.dto.EquineDto;
 import co.uk.bransby.equinetrainingtrackerapi.models.Equine;
+import co.uk.bransby.equinetrainingtrackerapi.models.dto.EquineDto;
 import co.uk.bransby.equinetrainingtrackerapi.services.EquineService;
 import lombok.AllArgsConstructor;
 import org.modelmapper.ModelMapper;
@@ -22,13 +22,22 @@ public class EquineController {
     private final EquineService equineService;
     private final ModelMapper modelMapper;
 
+    private EquineDto mapToDto(Equine equine) {
+        return modelMapper.map(equine, EquineDto.class);
+    }
+
+    private Equine mapToEntity(EquineDto equineDto) {
+        return modelMapper.map(equineDto, Equine.class);
+    }
+
     @GetMapping({"{id}"})
-    public ResponseEntity<EquineDto> findEquine(@PathVariable Long id){
+    public ResponseEntity<EquineDto> findEquine(@PathVariable Long id) {
         HttpHeaders resHeaders = new HttpHeaders();
         return equineService.getEquine(id)
                 .map(equine -> new ResponseEntity<>(mapToDto(equine), resHeaders, HttpStatus.OK))
                 .orElse(new ResponseEntity<>(resHeaders, HttpStatus.NOT_FOUND));
     }
+
     @GetMapping
     public ResponseEntity<List<EquineDto>> findAllEquines() {
         List<EquineDto> allEquines = equineService.getAllEquines()
@@ -70,11 +79,43 @@ public class EquineController {
                 .orElse(new ResponseEntity<>(resHeaders, HttpStatus.NOT_FOUND));
     }
 
-    private EquineDto mapToDto(Equine equine){
-        return modelMapper.map(equine, EquineDto.class);
+    @PatchMapping("{equineId}/programmes/{programmeId}")
+    public ResponseEntity<?> assignProgrammeToEquine(@PathVariable Long equineId, @PathVariable Long programmeId) {
+        try {
+            Equine equine = equineService.assignEquineToProgramme(equineId, programmeId);
+            return ResponseEntity.ok().body(modelMapper.map(equine, EquineDto.class));
+        } catch (EntityNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        }
     }
 
-    private Equine mapToEntity(EquineDto equineDto){
-        return modelMapper.map(equineDto, Equine.class);
+    @PatchMapping("{equineId}/yards/{yardId}")
+    public ResponseEntity<?> assignYardToEquine(@PathVariable Long equineId, @PathVariable Long yardId) {
+        try {
+            Equine equine = equineService.assignEquineToYard(equineId, yardId);
+            return ResponseEntity.ok().body(modelMapper.map(equine, EquineDto.class));
+        } catch (EntityNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        }
+    }
+
+    @PatchMapping("{equineId}/categories/{categoryId}")
+    public ResponseEntity<?> assignCategoryToEquine(@PathVariable Long equineId, @PathVariable Long categoryId) {
+        try {
+            Equine equine = equineService.assignEquineToCategory(equineId, categoryId);
+            return ResponseEntity.ok().body(modelMapper.map(equine, EquineDto.class));
+        } catch (EntityNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        }
+    }
+
+    @PatchMapping("{equineId}/skills/{skillId}")
+    public ResponseEntity<?> assignSkillToEquine(@PathVariable Long equineId, @PathVariable Long skillId) {
+        try {
+            Equine equine = equineService.assignEquineASkill(equineId, skillId);
+            return ResponseEntity.ok().body(modelMapper.map(equine, EquineDto.class));
+        } catch (EntityNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        }
     }
 }
--- src/main/java/co/uk/bransby/equinetrainingtrackerapi/services/EquineService.java
+++ src/main/java/co/uk/bransby/equinetrainingtrackerapi/services/EquineService.java
@@ -1,26 +1,30 @@
 package co.uk.bransby.equinetrainingtrackerapi.services;
 
-import co.uk.bransby.equinetrainingtrackerapi.models.Equine;
-import co.uk.bransby.equinetrainingtrackerapi.repositories.EquineRepository;
+import co.uk.bransby.equinetrainingtrackerapi.models.*;
+import co.uk.bransby.equinetrainingtrackerapi.repositories.*;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
 import javax.persistence.EntityNotFoundException;
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
+@AllArgsConstructor
 @Service
 public class EquineService {
 
     private final EquineRepository equineRepository;
-
-    public EquineService(EquineRepository equineRepository){
-        this.equineRepository = equineRepository;
-    }
+    private final ProgrammeRepository programmeRepository;
+    private final YardRepository yardRepository;
+    private final CategoryRepository categoryRepository;
+    private final SkillRepository skillRepository;
 
     public List<Equine> getAllEquines(){
         return equineRepository.findAll();
     }
+
     public Optional<Equine> getEquine(Long id) {
         return equineRepository.findById(id);
     }
@@ -38,5 +42,51 @@ public class EquineService {
 
     public void deleteEquine(Long id){
         equineRepository.deleteById(id);
+    }
+
+    public Equine assignEquineToProgramme(Long equineId, Long programmeId) throws EntityNotFoundException {
+        return equineRepository.findById(equineId)
+                .map(equine -> {
+                    Programme programme = programmeRepository.findById(programmeId)
+                            .orElseThrow(() -> new EntityNotFoundException("No programme found with id: " + programmeId));
+                    equine.setProgramme(programme);
+                    return equineRepository.saveAndFlush(equine);
+                })
+                .orElseThrow(() -> new EntityNotFoundException("No equine found with id: " + equineId));
+    }
+
+    public Equine assignEquineToYard(Long equineId, Long yardId) throws EntityNotFoundException {
+        return equineRepository.findById(equineId)
+                .map(equine -> {
+                    Yard yard = yardRepository.findById(yardId)
+                            .orElseThrow(() -> new EntityNotFoundException("No yard found with id: " + yardId));
+                    equine.setYard(yard);
+                    return equineRepository.saveAndFlush(equine);
+                })
+                .orElseThrow(() -> new EntityNotFoundException("No equine found with id: " + equineId));
+    }
+
+    public Equine assignEquineToCategory(Long equineId, Long categoryId) throws EntityNotFoundException {
+        return equineRepository.findById(equineId)
+                .map(equine -> {
+                    Category category = categoryRepository.findById(categoryId)
+                            .orElseThrow(() -> new EntityNotFoundException("No category found with id: " + categoryId));
+                    equine.setCategory(category);
+                    return equineRepository.saveAndFlush(equine);
+                })
+                .orElseThrow(() -> new EntityNotFoundException("No equine found with id: " + equineId));
+    }
+
+    public Equine assignEquineASkill(Long equineId, Long skillId) {
+        return equineRepository.findById(equineId)
+                .map(equine -> {
+                    Skill skill = skillRepository.findById(skillId)
+                            .orElseThrow(() -> new EntityNotFoundException("No skill found with id: " + skillId));
+                    Set<Skill> skills = equine.getSkills();
+                    skills.add(skill);
+                    equine.setSkills(skills);
+                    return equineRepository.save(equine);
+                })
+                .orElseThrow(() -> new EntityNotFoundException("No equine found with id: " + equineId));
     }
 }
