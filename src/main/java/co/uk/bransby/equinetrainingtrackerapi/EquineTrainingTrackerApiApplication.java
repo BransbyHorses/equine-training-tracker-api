a8dd0e5d1348925b4debfeb8b366af0140b47f80
michaelthorpe <michael.thorpe@digital.homeoffice.gov.uk>
2022-08-05 11:44:53 +0100

    add jwt security configuration

--- src/main/java/co/uk/bransby/equinetrainingtrackerapi/EquineTrainingTrackerApiApplication.java
+++ src/main/java/co/uk/bransby/equinetrainingtrackerapi/EquineTrainingTrackerApiApplication.java
@@ -1,5 +1,6 @@
 package co.uk.bransby.equinetrainingtrackerapi;
 
+import org.hibernate.cfg.Environment;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ src/main/java/co/uk/bransby/equinetrainingtrackerapi/security/SecurityConfiguration.java
@@ -0,0 +1,54 @@
+package co.uk.bransby.equinetrainingtrackerapi.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
+
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
+public class SecurityConfiguration {
+
+    @Autowired
+    private Environment env;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .cors()
+                .and().csrf().disable()
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().authenticated())
+                .oauth2ResourceServer().jwt();
+
+        return httpSecurity.build();
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        return NimbusJwtDecoder.withJwkSetUri(
+                "https://cognito-idp.eu-west-2.amazonaws.com/" + env.getProperty("COGNITO_USER_POOL") + "/.well-known/jwks.json"
+        ).build();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+        configuration.setAllowedMethods(List.of("*"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
+}
