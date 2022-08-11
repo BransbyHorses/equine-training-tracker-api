20349b477fac1d26404b3d69de0660515981273f
michaelthorpe <michael.thorpe@digital.homeoffice.gov.uk>
2022-08-11 16:20:23 +0100

    remove unused import

--- src/main/java/co/uk/bransby/equinetrainingtrackerapi/EquineTrainingTrackerApiApplication.java
+++ src/main/java/co/uk/bransby/equinetrainingtrackerapi/EquineTrainingTrackerApiApplication.java
@@ -1,6 +1,5 @@
 package co.uk.bransby.equinetrainingtrackerapi;
 
-import org.hibernate.cfg.Environment;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
