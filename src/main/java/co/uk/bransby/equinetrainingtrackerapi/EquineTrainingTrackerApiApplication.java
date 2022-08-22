8b1ccfe5d01ac1e03bc8721a1c5a8408b9e9cf81
michaelthorpe <michael.thorpe@digital.homeoffice.gov.uk>
2022-08-22 12:56:49 +0100

    add allowed origins wildcard

--- src/main/java/co/uk/bransby/equinetrainingtrackerapi/EquineTrainingTrackerApiApplication.java
+++ src/main/java/co/uk/bransby/equinetrainingtrackerapi/EquineTrainingTrackerApiApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import springfox.documentation.oas.annotations.EnableOpenApi;
