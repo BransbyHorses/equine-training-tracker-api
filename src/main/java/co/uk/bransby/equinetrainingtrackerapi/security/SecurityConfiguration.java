1e2687ed4a02963c26d36cec52a5fad92a0d9730
michaelthorpe <michael.thorpe@digital.homeoffice.gov.uk>
2022-08-22 14:37:46 +0100

    add cors config to spring security

--- src/main/java/co/uk/bransby/equinetrainingtrackerapi/security/SecurityConfiguration.java
+++ src/main/java/co/uk/bransby/equinetrainingtrackerapi/security/SecurityConfiguration.java
@@ -6,6 +6,11 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 
 @EnableWebSecurity
@@ -29,6 +34,16 @@ public class SecurityConfiguration {
                 // handles 403 unauthorised exceptions
                 .accessDeniedHandler(new AppAccessDeniedHandler());
         return httpSecurity.build();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedMethods(Arrays.asList("*"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
 }
