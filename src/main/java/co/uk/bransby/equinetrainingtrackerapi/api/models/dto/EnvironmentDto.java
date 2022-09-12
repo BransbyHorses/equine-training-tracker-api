66738c39c312490ed1023c78c773dd29afe97688
michaelthorpe <michael.thorpe@digital.homeoffice.gov.uk>
2022-09-12 16:39:54 +0100

    remove lombok from EnvironmentDto

--- src/main/java/co/uk/bransby/equinetrainingtrackerapi/api/models/dto/EnvironmentDto.java
+++ src/main/java/co/uk/bransby/equinetrainingtrackerapi/api/models/dto/EnvironmentDto.java
@@ -1,9 +1,29 @@
 package co.uk.bransby.equinetrainingtrackerapi.api.models.dto;
 
-import lombok.Data;
-
-@Data
 public class EnvironmentDto {
     private Long id;
     private String name;
+
+    public EnvironmentDto() {}
+
+    public EnvironmentDto(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
