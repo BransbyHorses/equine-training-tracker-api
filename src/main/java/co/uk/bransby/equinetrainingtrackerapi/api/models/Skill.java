97dce06776ad24cff21a1cb72b32101eedbe5372
michaelthorpe <michael.thorpe@digital.homeoffice.gov.uk>
2022-09-23 08:26:37 +0100

    remove unused imports

--- src/main/java/co/uk/bransby/equinetrainingtrackerapi/api/models/Skill.java
+++ src/main/java/co/uk/bransby/equinetrainingtrackerapi/api/models/Skill.java
@@ -4,9 +4,7 @@ import lombok.*;
 import org.hibernate.Hibernate;
 
 import javax.persistence.*;
-import java.util.List;
 import java.util.Objects;
-import java.util.Set;
 
 @NoArgsConstructor
 @AllArgsConstructor
--- src/main/java/co/uk/bransby/equinetrainingtrackerapi/api/models/dto/EquineDto.java
+++ src/main/java/co/uk/bransby/equinetrainingtrackerapi/api/models/dto/EquineDto.java
@@ -3,7 +3,6 @@ package co.uk.bransby.equinetrainingtrackerapi.api.models.dto;
 import lombok.Data;
 
 import java.util.List;
-import java.util.Set;
 
 @Data
 public class EquineDto {
