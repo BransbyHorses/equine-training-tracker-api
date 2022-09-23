0f1afb466a4f9fe069018ca44db213561bd6ae76
michaelthorpe <michael.thorpe@digital.homeoffice.gov.uk>
2022-09-23 15:42:14 +0100

    deserialize ProgressCode enum

--- src/main/java/co/uk/bransby/equinetrainingtrackerapi/api/models/ProgressCode.java
+++ src/main/java/co/uk/bransby/equinetrainingtrackerapi/api/models/ProgressCode.java
@@ -1,15 +1,33 @@
 package co.uk.bransby.equinetrainingtrackerapi.api.models;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+@JsonFormat(shape = JsonFormat.Shape.OBJECT)
 public enum ProgressCode {
     NOT_ABLE("Not Able"),
-    JUST_STARTED("Just Started"),
-    OK_WITH_LIMITS("Ok With Limits"),
+    JUST_STARTED("Just started"),
+    OK_WITH_LIMITS("Ok with limits"),
     CONFIDENT("Confident");
 
     private final String string;
 
     ProgressCode(String string) {
         this.string = string;
+    }
+
+    public String getString() {
+        return string;
+    }
+
+    @JsonCreator
+    public static ProgressCode getProgressCode(String string) {
+        for(ProgressCode pc : ProgressCode.values()) {
+            if(pc.getString().equals(string)){
+                return pc;
+            }
+        }
+        return null;
     }
 
     @Override
